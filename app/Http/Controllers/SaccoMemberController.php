225cdfd3a1100670dd156b91ccca243900e6e75d
PriscillaCole <51949337+PriscillaCole@users.noreply.github.com>
2023-08-07 00:48:14 +0300

    memeber

--- app/Http/Controllers/SaccoMemberController.php
+++ app/Http/Controllers/SaccoMemberController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\SaccoMember;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use App\Utils\Utils;
+use App\Models\Utils;
 
 class SaccoMemberController extends Controller
 {
